b603ff82a03235da580ac5f95372036e0f263eee
Lex Childs <lex.childs@praeses.com>
2024-03-21 11:35:35 -0500

    setup eslint

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -1,9 +1,37 @@
 return {
 
   {
+    "nvimtools/none-ls.nvim",
+    lazy = false,
+    dependencies = {
+      "nvimtools/none-ls-extras.nvim",
+      "nvim-lua/plenary.nvim"
+    },
+    config = function()
+      local null_ls = require("null-ls")
+      local formatting = null_ls.builtins.formatting
+      --local diagnostics = null_ls.builtins.diagnostics
+
+      null_ls.setup( {
+        sources = {
+          require("none-ls.diagnostics.eslint_d"),
+          require("none-ls.formatting.eslint_d"),
+        }
+      })
+
+    end
+  },
+
+
+  {
     "neovim/nvim-lspconfig",
     lazy = false,
     opts = {},
+
+    -- dependencies = {
+    --   "nvimtools/none-ls.nvim",
+    -- },
+
     config = function()
 
       local lspconfig = require('lspconfig')
@@ -25,6 +53,48 @@ return {
 
     end
   },
+
+
+
+  {
+    "stevearc/conform.nvim",
+    event = { "BufWritePre" },
+    cmd = { "ConformInfo" },
+    -- keys = {
+    --   {
+    --     -- Customize or remove this keymap to your liking
+    --     "<leader>f",
+    --     function()
+    --       require("conform").format({ async = true, lsp_fallback = true })
+    --     end,
+    --     mode = "",
+    --     desc = "Format buffer",
+    --   },
+    -- },
+    -- Everything in opts will be passed to setup()
+    opts = {
+      -- Define your formatters
+      formatters_by_ft = {
+        -- lua = { "stylua" },
+        -- python = { "isort", "black" },
+        javascript = { { "prettierd", "prettier" } },
+      },
+      -- Set up format-on-save
+      format_on_save = { timeout_ms = 500, lsp_fallback = true },
+      -- Customize formatters
+      formatters = {
+        shfmt = {
+          prepend_args = { "-i", "2" },
+        },
+      },
+    },
+    init = function()
+      -- If you want the formatexpr, here is the place to set it
+      vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
+    end,
+  }
+
+
 
 
 }
