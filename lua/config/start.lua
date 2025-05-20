a5f890f74c448969d06342849826b0e13ca409b5
Lex Childs <lex.childs@praeses.com>
2025-05-20 10:15:55 -0500

    tweaks to settings

--- lua/config/start.lua
+++ lua/config/start.lua
@@ -1,5 +1,3 @@
-
-
 vim.g.mapleader = ","
 vim.g.maplocalleader = "\\"
 
@@ -15,17 +13,18 @@ vim.o.shiftwidth = 2
 vim.o.formatprg = "par  -w80"
 vim.o.termguicolors = true
 
-vim.opt.spell = true
-vim.opt.autoread = true
-vim.opt.signcolumn = "yes"
+vim.o.ignorecase = true
+vim.o.smartcase = true
+
+vim.o.spell = true
+vim.o.autoread = true
+vim.o.signcolumn = "yes"
 
 -- All new windows should get focus
 vim.api.nvim_create_augroup("AutoJumpToSplit", { clear = true })
 vim.api.nvim_create_autocmd("WinNew", {
-  group = "AutoJumpToSplit",
-  callback = function()
-    vim.cmd('wincmd w')
-  end,
+	group = "AutoJumpToSplit",
+	callback = function()
+		vim.cmd('wincmd w')
+	end,
 })
-
-
