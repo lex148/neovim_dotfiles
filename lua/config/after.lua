318d068ebd06118c5831119f18641ee91c409fda
Lex Childs <lexchilds@gmail.com>
2025-05-24 07:58:40 -0500

    save format

--- lua/config/after.lua
+++ lua/config/after.lua
@@ -3,15 +3,20 @@ vim.cmd [[colorscheme tokyonight-storm]]
 --vim.cmd [[colorscheme gruvbox-material]]
 
 
+-- format the buffer on save with the LSP if it supports it
+vim.api.nvim_create_autocmd('LspAttach', {
+	callback = function(args)
+		local client = vim.lsp.get_client_by_id(args.data.client_id)
+		if not client then return end
 
--- call format whenever the file saves
-vim.api.nvim_create_autocmd("BufWritePre", {
-	callback = function()
-		local mode = vim.api.nvim_get_mode().mode
-		local filetype = vim.bo.filetype
-		if vim.bo.modified == true and mode == 'n' and filetype ~= "oil" then
-			vim.cmd('lua vim.lsp.buf.format()')
-		else
+		---@diagnostic disable-next-line: missing-parameter
+		if client.supports_method('textDocument/formatting') then
+			vim.api.nvim_create_autocmd('BufWritePre', {
+				buffer = args.buf,
+				callback = function()
+					vim.lsp.buf.format({ bufnr = args.buf, id = client.id })
+				end
+			})
 		end
-	end
+	end,
 })
