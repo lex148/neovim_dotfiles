d0d181b1d0c5749bdd37cf1cfa8d00e129040c26
Lex Childs <lexchilds@gmail.com>
2025-10-10 07:01:55 -0500

    fixed for nvim 11

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -13,10 +13,12 @@ return {
 		},
 		config = function()
 			local capabilities = require("blink.cmp").get_lsp_capabilities()
-			require("lspconfig").lua_ls.setup({ capabilities = capabilities })
-			require("lspconfig").tailwindcss.setup({ capabilities = capabilities })
-			require("lspconfig").ts_ls.setup({ capabilities = capabilities })
-			require("lspconfig").cssls.setup({ capabilities = capabilities })
+
+			vim.lsp.config('lua_ls', { capabilities = capabilities })
+			vim.lsp.config('tailwindcss', { capabilities = capabilities })
+			vim.lsp.config('ts_ls', { capabilities = capabilities })
+			vim.lsp.config('cssls', { capabilities = capabilities })
+
 			-- note: rust is loaded with rustaceanvim
 		end,
 	},
